f5442fa535489fa2149a45fd2f093e16e51002a8
manulopez14 <mlopez14@ieszaidinvergeles.org>
2023-06-03 16:32:21 +0200

    notification_service done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
@@ -36,47 +38,33 @@ class _MyHomePageState extends State<MyHomePage> {
   var _latitude = "";
   var _longitude = "";
   var _distanceInMeters = "";
-  
-  Future<Position> _determinePosition() async {
-  bool serviceEnabled;
-  LocationPermission permission;
+  StreamSubscription<Position>? _positionStreamSubscription;
 
-  // Test if location services are enabled.
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    // Location services are not enabled don't continue
-    // accessing the position and request users of the 
-    // App to enable the location services.
-    return Future.error('Location services are disabled.');
+  @override
+  void initState() {
+    super.initState();
+    _startLocationUpdates();
   }
 
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      // Permissions are denied, next time you could try
-      // requesting permissions again (this is also where
-      // Android's shouldShowRequestPermissionRationale 
-      // returned true. According to Android guidelines
-      // your App should show an explanatory UI now.
-      return Future.error('Location permissions are denied');
-    }
+  @override
+  void dispose() {
+    _stopLocationUpdates();
+    super.dispose();
   }
-  
-  if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever, handle appropriately. 
-    return Future.error(
-      'Location permissions are permanently denied, we cannot request permissions.');
-  } 
 
-  // When we reach here, permissions are granted and we can
-  // continue accessing the position of the device.
-  return await Geolocator.getCurrentPosition();
-}
+  Future<void> _startLocationUpdates() async {
+    _positionStreamSubscription = Geolocator.getPositionStream(
+    ).listen((position) {
+      _updatePosition(position);
+    });
+  }
 
-  Future<void> _updatePosition() async {
-    Position pos = await _determinePosition();
-    List pm = await placemarkFromCoordinates(pos.latitude, pos.longitude);
+  void _stopLocationUpdates() {
+    _positionStreamSubscription?.cancel();
+  }
+
+  Future<void> _updatePosition(Position pos) async {
+    List<Placemark> pm = await placemarkFromCoordinates(pos.latitude, pos.longitude);
     double distanceInMeters = await Geolocator.distanceBetween(pos.latitude, pos.longitude, 50.930455, 6.954859);
     setState(() {
       _latitude = pos.latitude.toString();
@@ -84,6 +72,8 @@ class _MyHomePageState extends State<MyHomePage> {
       _distanceInMeters = distanceInMeters.toString();
     });
   }
+  
+
 
   @override
   Widget build(BuildContext context) {
@@ -111,11 +101,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _updatePosition,
-        tooltip: 'Get the current position',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
